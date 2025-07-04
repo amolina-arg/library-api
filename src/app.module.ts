62bf6b3483afed7b3c363b7685e0402d0183ce2e
Andy Molina <andres.molina@bridgenext.com>
2025-07-04 17:58:44 -0300

    db connection updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,30 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     AuthModule,
     UsersModule,
-    DatabaseModule,
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DATABASE'),
+        entities: [User],
+        synchronize: configService.get('SYNCHRONIZE'),
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [],

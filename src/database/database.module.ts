b6cc8378962ccfc0fac9ccb824c2b3bfeeaa1302
Andy Molina <andres.molina@bridgenext.com>
2025-07-04 17:57:32 -0300

    deprecated files deleted

--- src/database/database.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { databaseProviders } from './database.providers';
-import { userProviders } from './repositories/users.providers';
-
-@Module({
-    providers: [...databaseProviders, ...userProviders],
-    exports: [...databaseProviders, ...userProviders],
-})
-export class DatabaseModule { }

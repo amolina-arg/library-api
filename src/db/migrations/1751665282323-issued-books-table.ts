0007f5f57e43dd889ea0196fc7e1de58b581049f
Andy Molina <andres.molina@bridgenext.com>
2025-07-10 16:51:11 -0300

    feat: :sparkles: support for states and return dates added

--- src/db/migrations/1751665282323-issued-books-table.ts
+++ src/db/migrations/1751665282323-issued-books-table.ts
@@ -4,10 +4,18 @@ import { TableForeignKey } from 'typeorm';
 export class IssuedBooksTable1751665282323 implements MigrationInterface {
 	public async up(queryRunner: QueryRunner): Promise<void> {
 		await queryRunner.query(
+			// eslint-disable-next-line quotes
+			`CREATE TYPE "issued_book_state_enum" AS ENUM('issued', 'returned', 'overdue')`,
+		);
+
+		await queryRunner.query(
 			`CREATE TABLE "issued_books" (
                 "id" uuid DEFAULT gen_random_uuid() PRIMARY KEY NOT NULL,
                 "book_id" uuid NOT NULL,
-                "user_id" uuid NOT NULL
+                "user_id" uuid NOT NULL,
+				"issued_at" timestamp NOT NULL DEFAULT now(),
+				"returned_at" timestamp,
+				"state" "issued_book_state_enum" NOT NULL DEFAULT 'issued'
             )`,
 		);
 		await queryRunner.createForeignKey(
@@ -46,5 +54,7 @@ export class IssuedBooksTable1751665282323 implements MigrationInterface {
 			}),
 		);
 		await queryRunner.query('DROP TABLE "issued_books"');
+		// Eliminar el tipo enum
+		await queryRunner.query("DROP TYPE 'issued_book_state_enum'");
 	}
 }
